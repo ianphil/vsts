f842564e2e8825112a6c6d21fa99512519dce2f4
tripdubroot <ian.philpot@microsoft.com>
2017-12-17 17:15:16 -0500

    refactored help text

---
+++ args/text_help.go
@@ -0,0 +1,27 @@
+package args
+
+const (
+	// DefaultErrorText - displayed when invalide commands are passed
+	DefaultErrorText string = "Please supply a valid command, -h, or --help switch"
+
+	// DefaultHelpText - displayed when no commands are passed
+	DefaultHelpText string = `Welcome to the vsts_cli
+
+                      $$$$$$$$$$$
+       ?????        $$$$$$$$ZZZ$$$
+    ~?++==++?I    7$$$$$$$$$$Z???Z$
+   ~?+==~~~=+I$Z 777$$$$$$$  +OOOOO$
+   ??+=        $77777$$$I     $ZZZZZ
+   7I?        II777777?       77$$$$
+    ~77      +IIIII777         777777
+     II~  =IIIIIIIIIZZ        III777
+      77IIIIIIIIII   77II?   IIIIII
+         7IIIII        $777777777     (TM)
+
+Microsoft (R) Visual Studio (R) Team Foundation Server
+
+    project     Manage VSTS projects
+    build       Manage VSTS builds
+    release 	Manage VSTS releases
+    `
+)
