4ee30351030901652420ee1413e3a941f93a3cfc
tripdubroot <ian.philpot@microsoft.com>
2017-12-17 17:15:46 -0500

    refactored commands

--- commands/command.go
+++ commands/command.go
@@ -2,18 +2,16 @@ package commands
 
 import (
 	"fmt"
-
-	m "github.com/tripdubroot/vsts/models"
 )
 
 // Exec executes the command passed by args
 func Exec(cmd string) {
-	var result m.ProjectList
-	result = execListProjects(cmd)
+	// var result models.ProjectList
+	// result = execListProjects(cmd)
 
-	for i := 1; i < result.Count; i++ {
-		fmt.Println(result.Value[i].Name)
-	}
+	// for i := 1; i < result.Count; i++ {
+	// 	fmt.Println(result.Value[i].Name)
+	// }
 
 	fmt.Println(cmd)
 }
---
+++ commands/top_level_cmds.go
@@ -0,0 +1,21 @@
+package commands
+
+const (
+	// Project top level command to manage projects
+	Project string = "project"
+
+	// Build top level command to manage builds
+	Build string = "build"
+
+	// Release top level command to manage releases
+	Release string = "release"
+
+	// Empty top level command to display default help
+	Empty string = "empty"
+
+	// ShHelp top level argument to display default help
+	ShHelp string = "-h"
+
+	// DhHelp top level command to display default help
+	DhHelp string = "--help"
+)
